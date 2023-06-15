44d3979de76d2a09e7eb97f6c983d78da1699758
JulianW051 <julianwessels1@gmail.com>
2023-06-15 17:05:18 +0200

    breeze install

---
+++ website/app/Models/Friend.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Friend extends Model
+{
+    protected $table = 'users';
+    // Assuming your table name is 'users'
+
+    // You can define any additional model relationships or methods here
+}
